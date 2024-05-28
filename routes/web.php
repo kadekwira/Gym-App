6ec5a48aff7d6e979199159229c393d315c45b61
Rasyid <10211001@student.itk.ac.id>
2024-05-28 23:42:27 +0800

    Change function dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', [DashboardController::class, 'dashboardAdmin']);
+Route::get('/dashboard', [DashboardController::class, 'dashboardadmin']);
