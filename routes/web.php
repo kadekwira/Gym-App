d979cd6175b4fb1415294f688916282503e90f3b
kadekwira <kadekw668@gmail.com>
2024-05-29 19:45:56 +0800

    create dashboard and data master

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
 
+Route::prefix('admin')->group(function(){
+  Route::get('/dashboard', [DashboardController::class, 'dashboardAdmin']);
+  Route::get('/data-admin',function(){
+    return view('admin.dataAdmin.index');
+  });
+  Route::get('/data-member',function(){
+    return view('admin.dataMember.index');
+  });
+  Route::get('/data-trainer',function(){
+    return view('admin.dataTrainer.index');
+  });
+  Route::get('/data-trial',function(){
+    return view('admin.dataTrial.index');
+  });
+  Route::get('/data-class',function(){
+    return view('admin.dataClass.index');
+  });
+  Route::get('/data-tips-trick',function(){
+    return view('admin.dataTips.index');
+  });
+  Route::get('/data-review',function(){
+    return view('admin.dataReview.index');
+  });
+  Route::get('/data-notif',function(){
+    return view('admin.dataNotif.index');
+  });
+});
 
-
-
-
-=======
-Route::get('/dashboard', [DashboardController::class, 'dashboardadmin']);
->>>>>>> origin/main
